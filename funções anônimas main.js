93a9d4593ad863166c966ef78cef4a992584d935
Leonardo <leo.braga@eaportal.org>
2025-04-26 13:40:40 -0300

    Arrow function, feita

--- funções anônimas main.js
+++ funções anônimas main.js
@@ -13,3 +13,24 @@ let somar = function(valor1, valor2) {
 
 console.log(mensagem())
 console.log(somar(10, 20))
+
+
+
+// Arrow function
+
+let mensagem = () => {
+    console.log("Olá, tudo bem?")
+
+}
+
+let somar = (valor1, valor2) =>  valor1 + valor2
+
+
+let raizQuadrada = valor1 =>  Math.sqrt(valor1)
+
+
+
+
+console.log(mensagem)
+console.log(somar(25, 800))
+console.log(raizQuadrada(4))
